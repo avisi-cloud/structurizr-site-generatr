a5f7db6face8caaf7695f508601982666bf5bca5
Jens Peters <jp7677@gmail.com>
2022-11-18 15:44:57 +0100

    Show progress bar during site updates
    Add a progress element when serving the site, make it visible and spinning
    when site updates are running.
    
    Also reorder javascript functions.

---
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/views/AutoReloading.kt
@@ -0,0 +1,37 @@
+package nl.avisi.structurizr.site.generatr.site.views
+
+import kotlinx.html.*
+import nl.avisi.structurizr.site.generatr.site.asUrlRelativeTo
+import nl.avisi.structurizr.site.generatr.site.model.PageViewModel
+
+fun HEAD.autoReloadScript(viewModel: PageViewModel) {
+    script(
+        type = ScriptType.textJavaScript,
+        src = "../" + "/auto-reload.js".asUrlRelativeTo(viewModel.url)
+    ) { }
+}
+
+fun BODY.updatingSiteProgressBar() {
+    div(classes = "is-overlay is-front-centered") {
+        progress(classes = "progress is-small mt-1 is-hidden") {
+            id = "updating-site"
+            value = "0"
+        }
+    }
+}
+
+fun BODY.updateSiteErrorHero() {
+    div(classes = "container is-hidden") {
+        id = "update-site-error"
+        div(classes = "hero is-danger mt-6") {
+            div(classes = "hero-body") {
+                p(classes = "title") {
+                    text("Error loading workspace file")
+                }
+                p(classes = "subtitle") {
+                    id = "update-site-error-message"
+                }
+            }
+        }
+    }
+}
