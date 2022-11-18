223359e55a9fc8e982ecf5eba8eafb1aeea9644b
dirkgroot <dirkgroot@users.noreply.github.com>
2022-11-18 09:38:06 +0000

    deploy: 8565c8ec07334c569941883297631a725837ee23

---
+++ auto-reload.js
@@ -0,0 +1,57 @@
+let hash = undefined;
+
+function reloadIfNeeded() {
+    fetch(window.location + "index.html.md5")
+        .then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText.toLowerCase())
+            }
+            return response.text()
+        })
+        .then((content) => {
+            if (!hash) {
+                hash = content;
+            } else if (hash !== content) {
+                console.log("Page content change detected, reloading ...")
+                location.reload()
+            } else {
+                console.log("Page content has not been changed.")
+            }
+        })
+        .catch((error) => {
+            if (error.message === "not found") {
+                location.href = location.origin
+            } else {
+                console.log(error)
+            }
+        });
+}
+
+function connectToWs() {
+    const ws = new WebSocket("ws://" + location.host + "/_events");
+    ws.onopen = () => {
+        console.log("Connected to socket.");
+        reloadIfNeeded();
+    }
+    ws.onclose = (event) => {
+        console.log('Socket has been closed. Attempting to reconnect ...', event.reason);
+        setTimeout(() => connectToWs(), 1000);
+    };
+    ws.onmessage = (event) => {
+        if (event.data === "site-updating") {
+            console.log("Site updating ...")
+        } else if (event.data === "site-updated") {
+            console.log("Site update detected, detect page content change ...")
+            document.getElementById("site").classList.remove("is-hidden")
+            document.getElementById("hero").classList.add("is-hidden")
+            reloadIfNeeded();
+        } else {
+            console.log(event.data)
+            document.getElementById("hero-subtitle").innerText = event.data
+            document.getElementById("site").classList.add("is-hidden")
+            document.getElementById("hero").classList.remove("is-hidden")
+        }
+    }
+}
+
+connectToWs();
