aee1813a11375f88a1d23fb7cbcc2f98ca583ab7
Dirk Groot <dirkgroot77@gmail.com>
2022-11-21 11:37:06 +0100

    Diagram zoom level is now maximized at 100%
    Diagrams that are wider than the available space in the browser window
    are shrunk to fit the available width. If enough space is available,
    then the diagrams are not zoomed in beyond 100%.

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/views/Diagram.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/views/Diagram.kt
@@ -4,7 +4,14 @@ import kotlinx.html.*
 import nl.avisi.structurizr.site.generatr.site.model.DiagramViewModel
 
 fun FlowContent.diagram(viewModel: DiagramViewModel) {
+    val diagramWidthInPixels = "viewBox=\"\\d+ \\d+ (\\d+) \\d+\"".toRegex()
+        .find(viewModel.svg)
+        ?.let { it.groupValues[1].toInt() }
+        ?: throw IllegalStateException("No viewBox attribute found in SVG!")
+
     figure {
+        style = "width: min(100%, ${diagramWidthInPixels}px);"
+
         unsafe {
             +viewModel.svg
         }
