6c21cf905b0d26cdb4b1c10d99a40a5cd686f3cd
Jens Peters <jp7677@gmail.com>
2023-02-22 08:27:15 +0100

    Simplify code

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/RelativeUrl.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/RelativeUrl.kt
@@ -3,13 +3,10 @@ package nl.avisi.structurizr.site.generatr.site
 import java.nio.file.Path
 import kotlin.io.path.relativeTo
 
+fun String.asUrlToDirectory(otherUrl: String) = "${this.asUrlToFile(otherUrl)}/"
+
 fun String.asUrlToFile(otherUrl: String) =
     if (otherUrl == this) "."
     else Path.of(this)
         .relativeTo(Path.of(otherUrl)).toString()
-
-fun String.asUrlToDirectory(otherUrl: String) =
-        if (otherUrl == this) "./"
-        else Path.of(this)
-                .relativeTo(Path.of(otherUrl)).toString() + "/"
 
