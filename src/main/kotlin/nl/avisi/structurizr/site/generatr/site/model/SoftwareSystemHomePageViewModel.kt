29533675000e0e1ebef49e14a9958e8428a24b18
Jens Peters <jp7677@gmail.com>
2023-02-27 10:29:13 +0100

    Hide internal properties
    The Structurizr DSL library introduced element/relationship identifiers
    as properties recently. Hide those for the site generation since we are
    not really interested in presenting them to users.
    
    Note that at the time of committing they still appear on the generated
    diagrams, though that has already been addressed upstream in
    com.structurizr:structurizr-export and will be fixed with a library
    update once a new version has been released.

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/SoftwareSystemHomePageViewModel.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/SoftwareSystemHomePageViewModel.kt
@@ -1,12 +1,13 @@
 package nl.avisi.structurizr.site.generatr.site.model
 
 import com.structurizr.model.SoftwareSystem
+import nl.avisi.structurizr.site.generatr.includedProperties
 import nl.avisi.structurizr.site.generatr.site.GeneratorContext
 
 class SoftwareSystemHomePageViewModel(generatorContext: GeneratorContext, softwareSystem: SoftwareSystem) :
     SoftwareSystemPageViewModel(generatorContext, softwareSystem, Tab.HOME) {
-    val hasProperties = softwareSystem.properties.any()
-    val propertiesTable = createPropertiesTableViewModel(softwareSystem.properties)
+    val hasProperties = softwareSystem.includedProperties.any()
+    val propertiesTable = createPropertiesTableViewModel(softwareSystem.includedProperties)
     val content = markdownToHtml(this, softwareSystem.info(), generatorContext.svgFactory)
 
     private fun SoftwareSystem.info() = documentation.sections
