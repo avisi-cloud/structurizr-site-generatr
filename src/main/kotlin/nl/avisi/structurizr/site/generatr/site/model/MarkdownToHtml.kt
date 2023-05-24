9ff587010c131b43eaa4111c4e996a9298fdcc8d
Uwe Bessle <uwe.bessle@iteratec.com>
2023-05-25 01:22:14 +0200

    handle PR comments from Dirk regarding MarkdownToHtml

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/MarkdownToHtml.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/MarkdownToHtml.kt
@@ -1,6 +1,5 @@
 package nl.avisi.structurizr.site.generatr.site.model
 
-import com.vladsch.flexmark.ext.tables.TablesExtension
 import com.vladsch.flexmark.html.HtmlRenderer
 import com.vladsch.flexmark.html.LinkResolver
 import com.vladsch.flexmark.html.LinkResolverFactory
@@ -9,7 +8,6 @@ import com.vladsch.flexmark.html.renderer.LinkStatus
 import com.vladsch.flexmark.html.renderer.ResolvedLink
 import com.vladsch.flexmark.parser.Parser
 import com.vladsch.flexmark.util.ast.Node
-import com.vladsch.flexmark.util.data.MutableDataSet
 import kotlinx.html.div
 import kotlinx.html.stream.createHTML
 import nl.avisi.structurizr.site.generatr.site.asUrlToFile
@@ -18,10 +16,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 fun markdownToHtml(pageViewModel: PageViewModel, markdown: String, svgFactory: (key: String, url: String) -> String?): String {
-    // val options = MutableDataSet()
-    
-    // options.set(Parser.EXTENSIONS, listOf(TablesExtension.create()))
-    val flexmarkConfig: FlexmarkConfig = pageViewModel.flexmarkConfig
+    val flexmarkConfig = pageViewModel.flexmarkConfig
     val options = flexmarkConfig.flexmarkOptions
     
     val parser = Parser.builder(options).build()
