1a2f682bb4a0c06f1af79269609f1ab486a2bed2
Uwe Bessle <uwe.bessle@iteratec.com>
2023-05-25 00:18:42 +0200

    handle even more PR comments from Dirk * cleanUp comments in MarkdownExtension.kt * drop deprecated unary `+` operator and use `unsafe` instead in MarkdownExtension.kt

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/views/MarkdownExtension.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/views/MarkdownExtension.kt
@@ -20,7 +20,6 @@ fun BODY.markdownAdmonitionScript(viewModel: PageViewModel) {
 
 fun HEAD.katexStylesheet() {
     // loading KaTeX as global on a webpage: https://katex.org/docs/browser.html#loading-as-global
-    // <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.6/dist/katex.min.css" integrity="sha384-mXD7x5S50Ko38scHSnD4egvoExgMPbrseZorkbE49evAfv9nNcbrXJ8LLNsDgh9d" crossorigin="anonymous">
     unsafe { 
         raw("""
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.6/dist/katex.min.css" integrity="sha384-mXD7x5S50Ko38scHSnD4egvoExgMPbrseZorkbE49evAfv9nNcbrXJ8LLNsDgh9d" crossorigin="anonymous">
@@ -30,7 +29,6 @@ fun HEAD.katexStylesheet() {
 
 fun HEAD.katexScript() {
     // loading KaTeX as global on a webpage: https://katex.org/docs/browser.html#loading-as-global
-    // <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.6/dist/katex.min.js" integrity="sha384-j/ZricySXBnNMJy9meJCtyXTKMhIJ42heyr7oAdxTDBy/CYA9hzpMo+YTNV5C+1X" crossorigin="anonymous"></script>
     unsafe { 
         raw("""
             <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.6/dist/katex.min.js" integrity="sha384-j/ZricySXBnNMJy9meJCtyXTKMhIJ42heyr7oAdxTDBy/CYA9hzpMo+YTNV5C+1X" crossorigin="anonymous"></script>
@@ -40,17 +38,6 @@ fun HEAD.katexScript() {
 
 fun HEAD.katexFonts() {
     // loading KaTeX as global on a webpage: https://katex.org/docs/browser.html#loading-as-global
-    // <script>
-    // window.WebFontConfig = {
-    //     custom: {
-    //     families: ['KaTeX_AMS', 'KaTeX_Caligraphic:n4,n7', 'KaTeX_Fraktur:n4,n7',
-    //         'KaTeX_Main:n4,n7,i4,i7', 'KaTeX_Math:i4,i7', 'KaTeX_Script',
-    //         'KaTeX_SansSerif:n4,n7,i4', 'KaTeX_Size1', 'KaTeX_Size2', 'KaTeX_Size3',
-    //         'KaTeX_Size4', 'KaTeX_Typewriter'],
-    //     },
-    // };
-    // </script>
-    // <script defer src="https://cdn.jsdelivr.net/npm/webfontloader@1.6.28/webfontloader.js" integrity="sha256-4O4pS1SH31ZqrSO2A/2QJTVjTPqVe+jnYgOWUVr7EEc=" crossorigin="anonymous"></script>
     unsafe {
         raw("""
             <script>
@@ -70,12 +57,9 @@ fun HEAD.katexFonts() {
 
 fun BODY.mermaidScript() {
     // Simple full example, how to include Mermaid: https://mermaid.js.org/config/usage.html#simple-full-example
-    // <script type="module">
-    //   import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
-    // </script>
-    script(
-        type = "module"
-    ) { 
-        +"import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';" 
+    script(type = "module") {
+        unsafe {
+            raw("import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';")
+        }
     }
 }
