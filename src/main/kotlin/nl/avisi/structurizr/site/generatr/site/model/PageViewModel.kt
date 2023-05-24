5ad7eacbc058768d93c1f36e8e3afa8ef36c93a1
Uwe Bessle <uwe.bessle@iteratec.com>
2023-05-25 01:34:20 +0200

    handle PR comments from Dirk regarding PageViewModel

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/PageViewModel.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/PageViewModel.kt
@@ -1,23 +1,22 @@
 package nl.avisi.structurizr.site.generatr.site.model
 
 import nl.avisi.structurizr.site.generatr.site.GeneratorContext
-import nl.avisi.structurizr.site.generatr.site.model.FlexmarkConfig
 
 abstract class PageViewModel(protected val generatorContext: GeneratorContext) {
     val pageTitle: String by lazy {
         if (pageSubTitle.isNotBlank() && generatorContext.workspace.name.isNotBlank())
-        "$pageSubTitle | ${generatorContext.workspace.name}"
+            "$pageSubTitle | ${generatorContext.workspace.name}"
         else if (generatorContext.workspace.name.isNotBlank())
-        generatorContext.workspace.name
+            generatorContext.workspace.name
         else
-        pageSubTitle
+            pageSubTitle
     }
     val favicon by lazy { FaviconViewModel(generatorContext, this) }
     val headerBar by lazy { HeaderBarViewModel(this, generatorContext) }
     val menu by lazy { MenuViewModel(generatorContext, this) }
     val includeAutoReloading = generatorContext.serving
     
-    val flexmarkConfig:FlexmarkConfig by lazy { 
+    val flexmarkConfig by lazy { 
         buildFlexmarkConfig(generatorContext) 
     }
     val includeAdmonition = flexmarkConfig.selectedExtensionMap.containsKey("Admonition")
