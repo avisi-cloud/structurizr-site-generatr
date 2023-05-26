578205509188740a48d414dc2e898db943f185b6
Kamil Gałuszka <kamil@flyrlabs.com>
2023-05-26 15:24:50 +0200

    Add improvement suggested by @dirkgroot

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/ContainerDecisionsTableViewModel.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/ContainerDecisionsTableViewModel.kt
@@ -1,13 +1,14 @@
 package nl.avisi.structurizr.site.generatr.site.model
 
 import com.structurizr.model.Container
+import nl.avisi.structurizr.site.generatr.hasDecisions
 
 fun PageViewModel.createContainerDecisionsTableViewModel(containers: Collection<Container>, hrefFactory: (Container) -> String) =
     TableViewModel.create {
         headerRow(headerCell("#"), headerCell("Container Decisions"))
         containers
             .sortedBy { it.name }
-            .filter { it.documentation.decisions.isNotEmpty() }
+            .filter { it.hasDecisions() }
             .forEachIndexed { index, container ->
                 bodyRow(
                     cellWithIndex((index+1).toString()),
