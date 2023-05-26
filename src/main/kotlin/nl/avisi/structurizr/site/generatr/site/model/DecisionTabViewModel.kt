91755e31e17f4823e4094490258a48d1107dfbbe
Kamil Gałuszka <galuszkak@gmail.com>
2023-05-26 15:20:19 +0200

    Add filtering for container with existing decisions.
    Co-authored-by: Dirk Groot <dirkgroot77@gmail.com>

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/DecisionTabViewModel.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/DecisionTabViewModel.kt
@@ -18,6 +18,7 @@ fun SoftwareSystemPageViewModel.createDecisionsTabViewModel(softwareSystem: Soft
         }
         softwareSystem
                 .containers
+                .filter { it.hasDecisions() }
                 .map {
                     DecisionTabViewModel(
                             this@createDecisionsTabViewModel,
