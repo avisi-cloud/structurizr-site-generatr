531214785932f09f234a1a38c16cda798f694e1b
Jens Peters <jp7677@gmail.com>
2023-10-18 14:49:09 +0200

    Fix link to custom stylesheet
    It needs to be relative to the current page.

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/CustomStylesheetViewModel.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/CustomStylesheetViewModel.kt
@@ -1,23 +1,21 @@
 package nl.avisi.structurizr.site.generatr.site.model
 
 import nl.avisi.structurizr.site.generatr.site.GeneratorContext
+import nl.avisi.structurizr.site.generatr.site.asUrlToFile
 
-class CustomStylesheetViewModel(generatorContext: GeneratorContext) {
-    val resourceURI = getResourceURI(generatorContext)
+class CustomStylesheetViewModel(generatorContext: GeneratorContext, pageViewModel: PageViewModel) {
+    val resourceURI = getResourceURI(generatorContext)?.let {
+        if (!it.lowercase().startsWith("http"))
+            "/$it".asUrlToFile(pageViewModel.url)
+        else
+            it
+    }
     val includeCustomStylesheet = resourceURI != null
 
-    private fun getResourceURI(generatorContext: GeneratorContext): String? {
-        val stylesheet = generatorContext.workspace.views.configuration.properties
+    private fun getResourceURI(generatorContext: GeneratorContext) =
+        generatorContext.workspace.views.configuration.properties
             .getOrDefault(
-            "generatr.style.customStylesheet",
-            null
-        )
-
-        if (stylesheet != null) {
-            return if (stylesheet.lowercase().startsWith("http")) stylesheet else "./$stylesheet"
-        }
-
-        return null
-    }
-
+                "generatr.style.customStylesheet",
+                null
+            )
 }
