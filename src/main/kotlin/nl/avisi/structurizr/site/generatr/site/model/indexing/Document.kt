1cf0f2e14232be8f3453af50ac955c5271e4812f
robgonfer <robgonfer@gmail.com>
2024-01-31 17:31:54 +0100

    Update Document.kt

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/indexing/Document.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/indexing/Document.kt
@@ -3,4 +3,9 @@ package nl.avisi.structurizr.site.generatr.site.model.indexing
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Document(val href: String, val type: String, val title: String, val text: String)
+data class Document(val href: String, val type: String, val title: String, val text: String, val id: Int = index())
+
+val index: () -> Int = object {
+    var x = 0
+    fun createFunc() = fun() = x++
+}.createFunc()
