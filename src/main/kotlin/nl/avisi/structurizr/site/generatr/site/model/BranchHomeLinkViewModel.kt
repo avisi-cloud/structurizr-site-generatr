7539c22f78f5e49b19acccb49b3fde4ece7cfdc1
Victor Levasseur <victor.levasseur@glady.com>
2024-04-18 18:49:13 +0200

    fix: the header branch selector now properly encode the branch name as an URI

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/BranchHomeLinkViewModel.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/model/BranchHomeLinkViewModel.kt
@@ -1,11 +1,13 @@
 package nl.avisi.structurizr.site.generatr.site.model
 
 import nl.avisi.structurizr.site.generatr.site.asUrlToDirectory
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 data class BranchHomeLinkViewModel(
     private val pageViewModel: PageViewModel,
     private val branchName: String
 ) {
     val title get() = branchName
-    val relativeHref get() = HomePageViewModel.url().asUrlToDirectory(pageViewModel.url) + "../$branchName/"
+    val relativeHref get() = HomePageViewModel.url().asUrlToDirectory(pageViewModel.url) + "../${URLEncoder.encode(branchName, StandardCharsets.UTF_8)}/"
 }
