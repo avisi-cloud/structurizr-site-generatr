baaa68567022a5023da38964c54b159f9ded863d
Jens Peters <jens.peters@avisi.nl>
2025-03-13 09:28:16 +0100

    Open external links in new tab

--- src/main/kotlin/nl/avisi/structurizr/site/generatr/site/views/ExternalLink.kt
+++ src/main/kotlin/nl/avisi/structurizr/site/generatr/site/views/ExternalLink.kt
@@ -3,11 +3,11 @@ package nl.avisi.structurizr.site.generatr.site.views
 import kotlinx.html.FlowOrPhrasingContent
 import kotlinx.html.a
 import nl.avisi.structurizr.site.generatr.site.model.ExternalLinkViewModel
-import nl.avisi.structurizr.site.generatr.site.model.LinkViewModel
 
 fun FlowOrPhrasingContent.externalLink(viewModel: ExternalLinkViewModel) {
     a(
         href = viewModel.href,
+        target = "_blank",
     ) {
         +viewModel.title
     }
